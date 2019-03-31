372dd1156c804933570039342c891cd3426d510d
James <jesullivan222@gmail.com>
2019-03-31 16:05:56 -0400

    Completed RecursionPractice.java, with comments.

--- src/RecursionPractice.java
+++ src/RecursionPractice.java
@@ -12,27 +12,45 @@ public class RecursionPractice {
         return 0;       // base case
     }
 
-    // This method is not recursive
-    private static void findFixedSumPairs(int[] a, int k){
-        int index = 0;
-        if (index < a.length-2){
-            recursiveFixedSumPairs(a, k, index, index+1);
+
+    /**
+     * Calls recursiveFixedSumPairs to print out all sum pairs within a that are equal to k
+     * @param a: int array (must have >1 element, sorted in ascending order)
+     * @param k: int used to compare against sum pair values within 'a'
+     */
+    public static void findFixedSumPairs(int[] a, int k){
+        if (a.length > 1){
+            recursiveFixedSumPairs(a, k, 0, 1);     // starts by comparing values at index 0 and 1
+        }else{
+            System.out.println("int array 'a' must contain at least 2 values. Invalid array.");
         }
     }
 
-    // This method is recursive
+
+    /**
+     * Recursively iterates through int array 'a', prints all sum pairs within 'a' equal to k
+     * @param a: int array (must have >1 element, sorted in ascending order)
+     * @param k: int used to compare against sum pairs within 'a'
+     * @param i: int index used to represent 1st int in sum pair
+     * @param j: int index used to represent 2nd int in sum pair
+     */
     private static void recursiveFixedSumPairs(int[] a, int k, int i, int j){
         int sum;
-        sum = a[i] + a[j];      // need to rearrange to avoid index out of bounds at start
-        if (sum == k) {         // add more under this if statement. Currently it ends after one print.
+        sum = a[i] + a[j];
+        if (sum == k) {     // prints index and values of pair, increments i if possible
             System.out.println("a[" + i + "] = " + a[i] + ", a[" + j + "] = " + a[j]);
-        }else if(sum < k){
-            if (j < a.length - 1){
-                recursiveFixedSumPairs(a, k, i, j+1);
-            }else if (i < a.length -2){
+            if (i < a.length - 2){
                 recursiveFixedSumPairs(a, k, i+1, i+2);
             }
-        }
+        }else if(sum < k){
+            if (j < a.length - 1){      // if there are still j values to iterate over, increment j
+                recursiveFixedSumPairs(a, k, i, j+1);
+            }else if (i < a.length -2){     // exhausted all combos of i+j at current i index, increment i
+                recursiveFixedSumPairs(a, k, i+1, i+2);
+            }
+        }else if (i < a.length - 2){    // assumes sum > k, increment i
+                recursiveFixedSumPairs(a, k, i+1, i+2);
+        }       // base case: sum > k && i > a.length - 2
     }
 
 
@@ -46,7 +64,7 @@ public class RecursionPractice {
         System.out.println("\nFixed sum pairs: ");
         int[] a = {1, 5, 8, 11, 12, 14, 15, 20, 21, 22, 23, 25, 28, 30, 34, 36};
         int k;
-        k = 13;
+        k = 43;     // .zip file initially set this to 60, which had no equivalent sum pairs
         System.out.print("k = " + k + "\na = [");
         for (int i=0; i<a.length-1; i++){
             System.out.print(a[i] + ", ");
