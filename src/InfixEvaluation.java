a7f54c0121b56665ecd5e1548aa8627c5c2830f5
james.sullivan <jesullivan222@gmail.com>
2019-04-01 11:19:46 -0400

    InfixEvaluation.java complete and working. Minor changes and comments necessary.

--- src/InfixEvaluation.java
+++ src/InfixEvaluation.java
@@ -1,66 +1,57 @@
 public class InfixEvaluation {
 
     public static int evaluate(String expression){
-        final String opening = "(";
-        final String closing = ")";
-        final String operatorString = "+-*/";
+        final String opening = "(";                             // String matching opening parenthesis
+        final String closing = ")";                             // String matching closing parenthesis
+        final String operatorString = "+-*/";                   // String of possible operators
 
-        LinkedStack <Character> buffer = new LinkedStack<>();
-        LinkedStack <Integer> operand = new LinkedStack<>();
-        LinkedStack <Character> operator = new LinkedStack<>();
+        LinkedStack <String> buffer = new LinkedStack<>();      // stack for opening parentheses
+        LinkedStack <Integer> operand = new LinkedStack<>();    // stack for operand integers
+        LinkedStack <String> operator = new LinkedStack<>();    // stack for operators
 
-        for (char c : expression.toCharArray()){
-            /*
-            System.out.println(c);     // for testing only, delete
-            System.out.println("\nBuffer: " + buffer.toString());
-            System.out.println("operand: " + operand.toString());
-            System.out.println("operator: " + operator.toString() + "\n");
-            */
+        String[] splitExpression = expression.split(" ");       // splits expression string by spaces
 
-            if (opening.indexOf(c) != -1)
+        for (String c : splitExpression){
+
+            if (opening.contains(c))                // check if String is an opening parenthesis
                 buffer.push(c);
-            else if (operatorString.indexOf(c) != -1)
+            else if (operatorString.contains(c))    // check if String is an operator
                 operator.push(c);
-            else if (closing.indexOf(c) != -1){
-                if (operator.top().equals('+')){
-                    operator.pop();
-                    operand.push(operand.pop() + operand.pop());
-                }else if (operator.top().equals('-')){
-                    operator.pop();
-                    operand.push(operand.pop() - operand.pop());
-                }else if (operator.top().equals('*')){
-                    operator.pop();
-                    operand.push(operand.pop() * operand.pop());
-                }else if (operator.top().equals('/')){
-                    operator.pop();
-                    operand.push(operand.pop() / operand.pop());
-                }
+            else if (closing.contains(c)){          // check if String is a closing parenthesis
+                int operand2 = operand.pop();       // top of operand stack is 2nd operand
+                int operand1 = operand.pop();       // next in operand stack is 1st operand
 
-                /*
-                else if (operator.pop().equals('-'))
-                    operand.push(operand.pop() - operand.pop());
-                else if (operator.pop().equals('*'))
-                    operand.push(operand.pop() * operand.pop());
-                else if (operator.pop().equals('/'))
-                    operand.push(operand.pop() / operand.pop());
-                */
+
+                if (operator.top().equals("+")){        // can be changed to a switch statement
+                    operator.pop();
+                    operand.push(operand1 + operand2);
+                }else if (operator.top().equals("-")){
+                    operator.pop();
+                    operand.push(operand1 - operand2);
+                }else if (operator.top().equals("*")){
+                    operator.pop();
+                    operand.push(operand1 * operand2);
+                }else if (operator.top().equals("/")){
+                    operator.pop();
+                    operand.push(operand1 / operand2);
+                }
 
                 buffer.pop();
             }
-            else
-                operand.push(Character.getNumericValue(c));
+            else                                    // assumes String is a number
+                operand.push(Integer.parseInt(c));  // converts to int & pushes to operand stack
         }
         return (operand.pop());
     }
 
     public static void main(String[] args) {
-        String s1 = "((2+5)*3)";
+        String s1 = "( ( 2 + 5 ) * 3 )";
         System.out.println(evaluate(s1));
 
-        String s2 = "(((3+2)*3)-(6/2))";
+        String s2 = "( ( ( 3 + 2 ) * 3 ) - ( 6 / 2 ) )";
         System.out.println(evaluate(s2));
 
-        String s3 = "(((7-5)*2)/(10-8))";
+        String s3 = "( ( ( 7 - 5 ) * 2 ) / (  10 - 8 ) )";
         System.out.println(evaluate(s3));
     }
 }
