d1140e0f42fc5304e88dd0bedd6c3bdddf029e8f
James <jesullivan222@gmail.com>
2019-04-01 22:51:46 -0400

    Completed InfixEvaluation.java.

--- src/InfixEvaluation.java
+++ src/InfixEvaluation.java
@@ -1,33 +1,39 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class InfixEvaluation {
 
+    /**
+     * Evaluates arithmetic expression using stacks, returns int value
+     * @param expression: String with valid arithmetic expression
+     * @return operandStack.pop(): int evaluation of expression returned
+     */
     public static int evaluate(String expression){
         final String opening = "(";                             // String matching opening parenthesis
         final String closing = ")";                             // String matching closing parenthesis
         final String operatorString = "+-*/";                   // String of possible operators
 
-        LinkedStack <String> buffer = new LinkedStack<>();      // stack for opening parentheses
         LinkedStack <Integer> operandStack = new LinkedStack<>();    // stack for operand integers
         LinkedStack <String> operatorStack = new LinkedStack<>();    // stack for operators
 
         String[] splitExpression = expression.split(" ");       // splits expression string by spaces
 
-        for (String c : splitExpression){
+        for (String s : splitExpression){
 
-            if (opening.contains(c))                // check if String is an opening parenthesis
-                buffer.push(c);
-            else if (operatorString.contains(c))    // check if String is an operator
-                operatorStack.push(c);
-            else if (closing.contains(c)){          // check if String is a closing parenthesis
+            if (s.equals(opening))                // check if String is an opening parenthesis (does nothing)
+                continue;
+            else if (operatorString.contains(s))    // check if String is an operator
+                operatorStack.push(s);
+            else if (closing.contains(s)){          // check if String is a closing parenthesis
                 int operand2 = operandStack.pop();       // top of operand stack is 2nd operand
                 int operand1 = operandStack.pop();       // next in operand stack is 1st operand
 
                 String operator = operatorStack.top();      // Assigns most recent operator in operatorStack
                 switch(operator){
                     case "+":
-                        operatorStack.pop();
-                        operandStack.push(operand1 + operand2);
+                        operatorStack.pop();                                // pops most recent operator
+                        operandStack.push(operand1 + operand2);     // pushes evaluated value to operandStack
                         break;
                     case "-":
                         operatorStack.pop();
@@ -42,27 +48,43 @@ public class InfixEvaluation {
                         operandStack.push(operand1 / operand2);
                         break;
                 }
-
-                buffer.pop();
             }
-            else                                    // assumes String is a number
-                operandStack.push(Integer.parseInt(c));  // converts to int & pushes to operand stack
+            else{     // assuming valid input string, this will be a number
+                operandStack.push(Integer.parseInt(s));     // converts String to int & pushes to operand stack
+            }
+
+
         }
-        return (operandStack.pop());
+        return (operandStack.pop());                    // after all calculations, returns final value
     }
 
     public static void main(String[] args) {
+        String lineFromFile;
 
-        FileInputStream expressionFile = new FileInputStream("/resources/infix_expressions.txt"); // fix this
+        try{
+            FileInputStream expressionFile = new FileInputStream("resources/infix_expressions.txt");
+            Scanner fileInput = new Scanner(expressionFile);
 
+            while (fileInput.hasNextLine()){                // iterates through lines in txt file
+                lineFromFile = fileInput.nextLine();        // assumes all lines are valid
 
-        String s1 = "( ( 2 + 5 ) * 3 )";
-        System.out.println(evaluate(s1));
+                // Prints expression and output of expression from each line of file
+                System.out.println("The value of " + lineFromFile + " is " + evaluate(lineFromFile));
+            }
+        }
 
-        String s2 = "( ( ( 3 + 2 ) * 3 ) - ( 6 / 2 ) )";
-        System.out.println(evaluate(s2));
+        // fileNoteFoundException error
+        catch (FileNotFoundException ex){
+            System.err.println(ex.getMessage() + ": File not found. Exiting.");
+            ex.printStackTrace();
+            System.exit(0);
+        }
 
-        String s3 = "( ( ( 7 - 5 ) * 2 ) / (  10 - 8 ) )";
-        System.out.println(evaluate(s3));
+        // all other exceptions, catch-all
+        catch (Exception ex){
+            System.err.println(ex.getMessage());
+            ex.printStackTrace();
+            System.exit(0);
+        }
     }
 }
